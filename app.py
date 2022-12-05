1ac3ba0e906ced2d2b785822649fb0615a85f285
LachJack97 <111512243+LachJack97@users.noreply.github.com>
2022-12-05 22:03:13 +1000

    Reworked system. New log in. New actions.

--- app.py
+++ app.py
@@ -1,26 +1,39 @@
+username = ""
+user_state = "Logged Out"
+actions = ['New Entry', 'Delete Entry', 'Log Out']
+user_action = ""
+journal_entry = []
+def log_in():
+        
+        user = input("Are you a new user here?   ")
+        global user_state
 
-password = ""
-password_confim = ""
+        if user == 'no' or 'yes':
+                username = input("Please enter a new username here...")
+                print(username)
+                user_state = 'Logged in'
+        else:
+                print("Please enter your username here")
+                print(username)
+                user_state = 'Logged in'
 
-print("Welcome to your personal journal")
 
-def password_creation():
-        password=input("You do not currently have a password set. Please enter your new password here.   ")
-        password_confirm=input("Please enter your password again to confirm.   "),
 
-if password == "":
-    password_creation()
-else:
-    input("Please enter your password here.   ")    
+while user_state == 'Logged Out':
+        print("Welcome to your personal journal")
 
-print(password_confim)
-print(password)
+        log_in()
 
-while password_confim != password:
-        print("The two passwords you have entered are not the same, please try again")
-        password_creation()
-
-if password_confim == password:
-        password = password_confim
-        print(password)
+        while user_state == 'Logged in':
+                print('What would you like to do?', actions)
+                user_action = input('Please choose one of the options from above....')
+                if user_action == actions[0]:
+                        new_journal = input('Please enter the date for this entry')                                
+                        journal_entry.append(new_journal)
+                        print(journal_entry[-1])
+                        print(journal_entry)
+                elif user_action == actions[2]:
+                        user_state = 'Logged Out'
+                break
+        
 
