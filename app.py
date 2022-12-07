2fe0c186b7937da7ddc9d882b261f157bbefc80f
LachJack97 <111512243+LachJack97@users.noreply.github.com>
2022-12-07 22:43:55 +1000

    Added more menu actions and functionality

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-username = ""
+import datetime
+
+
+# Lists
+actions = ['New Entry', 'Delete Entry', 'Log Out', 'Read Entry']
+journal_entry_titles = []
+journal_entry_list = []
+
+# Element Storage
 user_state = "Logged Out"
-actions = ['New Entry', 'Delete Entry', 'Log Out']
+username = ""
 user_action = ""
-journal_entry = []
+journal_entry = ""
+delete_journal_entry = ""
+read_journal_entry = ""
+
+
 def log_in():
         
         user = input("Are you a new user here?   ")
@@ -18,6 +30,42 @@ def log_in():
                 user_state = 'Logged in'
 
 
+def new_journal_entry():
+        user_state = 'journal_entry'
+        print(user_state)
+        new_journal = input("Please enter a title")                                
+        journal_entry_titles.append(new_journal)
+        print(journal_entry_titles[-1])
+        journal_entry = input("Please write your journal entry here...")
+        journal_entry_list.append(journal_entry)
+
+def read_journal():
+        user_state = 'read_journal_entry'
+        print(user_state)
+        print("Which journal entry would you like to read?")
+        print(journal_entry_titles)
+        read_journal_entry = input("")
+        if read_journal_entry == journal_entry_titles[0]:
+                print(journal_entry_list[0])
+        elif read_journal_entry == journal_entry_titles[1]:
+                print(journal_entry_list[1])
+        elif read_journal_entry == journal_entry_titles[1]:
+                print(journal_entry_list[1])
+        
+
+def journal_delete():
+        print("Which journal entry would you like to delete?")
+        print(journal_entry_titles)
+        delete_journal_entry = input("")
+        if delete_journal_entry == journal_entry_titles[0]:
+                del journal_entry_titles[0]
+                del journal_entry_list[0]
+        elif delete_journal_entry == journal_entry_titles[1]:
+                del journal_entry_titles[1]
+                del journal_entry_list[1]
+        elif delete_journal_entry == journal_entry_titles[2]:
+                del journal_entry_titles[2]
+                del journal_entry_list[2]
 
 while user_state == 'Logged Out':
         print("Welcome to your personal journal")
@@ -28,12 +76,16 @@ while user_state == 'Logged Out':
                 print('What would you like to do?', actions)
                 user_action = input('Please choose one of the options from above....')
                 if user_action == actions[0]:
-                        new_journal = input('Please enter the date for this entry')                                
-                        journal_entry.append(new_journal)
-                        print(journal_entry[-1])
-                        print(journal_entry)
-                elif user_action == actions[2]:
-                        user_state = 'Logged Out'
-                break
+                        new_journal_entry()
+                
+                if user_action == actions[1] and journal_entry_titles != []:
+                        journal_delete()
+
+                if user_action == actions[3] and journal_entry_titles != []:
+                        read_journal()
         
+                if user_action == actions[2]:
+                        user_state = 'Logged Out'
+                
+                
 
